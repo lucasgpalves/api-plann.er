92f66d2ee2f2d1882633288420f1b02ba62ef1e6
Lucas Alves <alvez.lucas2702@gmail.com>
2024-07-10 18:58:34 -0300

    feat: participant repository

---
+++ src/main/java/com/rocketseat/planner/participant/ParticipantRepository.java
@@ -0,0 +1,9 @@
+package com.rocketseat.planner.participant;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ParticipantRepository extends JpaRepository<Participant, UUID>{
+    
+}
